733e066342489abb6867e9f3c96f1967b628a7fb
Robert Miller <robertmiller@Roberts-MacBook-Pro-2.local>
2018-04-09 22:22:11 -0400

    initial

---
+++ New_Attendance_App/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  New_Attendance_App
+//
+//  Created by Robert Miller on 4/9/18.
+//  Copyright © 2018 Robert Miller. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goToLoginScreen(_ sender:Any){
+        self.performSegue(withIdentifier: "toLoginScreen", sender:self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ New_Attendance_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New_Attendance_App
+//
+//  Created by Robert Miller on 4/9/18.
+//  Copyright © 2018 Robert Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
