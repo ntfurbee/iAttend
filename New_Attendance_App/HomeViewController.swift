a749e5437414f4ab8c63602b12d1190137b511d0
Robert Miller <robertmiller@Roberts-MacBook-Pro-2.local>
2018-04-11 10:24:07 -0400

    update

--- New_Attendance_App/HomeViewController.swift
+++ New_Attendance_App/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
 
     @IBAction func goToLoginScreen(_ sender:Any){
